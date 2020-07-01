552519cc78e2aa019067c05653aced879568b934
Alexei Karve <karve@us.ibm.com>
2020-07-01 12:38:32 -0400

    ansible-role-socks5-tunnel with ssh passphrase

--- Jumphosts/roles/ansible-role-socks5-tunnel/tasks/jumphostlogin.sh
+++ Jumphosts/roles/ansible-role-socks5-tunnel/tasks/jumphostlogin.sh
@@ -5,9 +5,9 @@ if ! [[ $jh1_socks_port =~ $re ]] ; then
     #echo "Socks port is not a number" >&2
     dparam=`echo $jh1_socks_port | sed "s|^socks.*://||" | sed "s|^unixsocket||"`
 else
-    dparam=/tmp/mysocks$jh1_socks_port
+    #dparam=/tmp/mysocks$jh1_socks_port
+    dparam=127.0.0.1:$jh1_socks_port
 fi
-#dparam=127.0.0.1:$jh1_socks_port
 
 if [ ! -z "$jh5_ip" ]; then
     ssh -i $jh5_ssh_private_key -oPubkeyAuthentication=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oProxyCommand="ssh -i $jh4_ssh_private_key -W $jh5_ip:$jh5_ssh_port -oPubkeyAuthentication=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oProxyCommand=\"ssh -i $jh3_ssh_private_key -W $jh4_ip:$jh4_ssh_port -oPubkeyAuthentication=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oProxyCommand=\\\"ssh -i $jh2_ssh_private_key -W $jh3_ip:$jh3_ssh_port -oPubkeyAuthentication=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oProxyCommand=\\\\\\\"ssh -i $jh1_ssh_private_key -W $jh2_ip:$jh2_ssh_port -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -p $jh1_ssh_port $jh1_ssh_user@$jh1_ip\\\\\\\" -p $jh2_ssh_port $jh2_ssh_user@$jh2_ip\\\" -p $jh3_ssh_port $jh3_ssh_user@$jh3_ip\" -p $jh4_ssh_port $jh4_ssh_user@$jh4_ip" -p $jh5_ssh_port $jh5_ssh_user@$jh5_ip -fN -D $dparam
