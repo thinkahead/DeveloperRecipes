aef19b4da050e005f18fe948b19d08e31d1a329b
Alexei Karve <karve@us.ibm.com>
2020-07-07 15:04:51 -0400

    unix domain socket file

--- Jumphosts/windowstest_with_tunnel_unixfile.py
+++ Jumphosts/windowstest_with_tunnel_unixfile.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Run command on Windows host')
 parser.add_argument("--host", required=True)
 parser.add_argument("--port", default=5985)
-parser.add_argument("--socksport", default=1234)
+parser.add_argument("--socksport", default='socks5h://unixsocket/tmp/mysock.sock')
 parser.add_argument("--username", required=True)
 parser.add_argument("--password", required=True)
 parser.add_argument("--protocol", default="http")
@@ -19,13 +19,15 @@ p = Protocol(
     username=args.username,
     password=args.password,
     server_cert_validation='ignore',
-    proxy='socks5h://localhost:'+str(args.socksport))
+    proxy=args.socksport)
 shell_id = p.open_shell()
 
 command_id = p.run_command(shell_id, 'ipconfig', ['/all'])
 std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
 p.cleanup_command(shell_id, command_id)
 p.close_shell(shell_id)
-print(status_code)
-print(std_out)
-print(std_err)
+print("status_code:",status_code)
+print("std_out:")
+print(str(std_out, 'ascii')) # python3 only
+print("std_err:")
+print(str(std_err, 'ascii')) # python3 only
--- Jumphosts/windowstest_with_tunnel_unixfile2.py
+++ Jumphosts/windowstest_with_tunnel_unixfile2.py
@@ -20,7 +20,7 @@ encoded_cmd = b64encode(cmd.encode('utf_16_le')).decode('ascii')
 parser = argparse.ArgumentParser(description='Run command on Windows host')
 parser.add_argument("--host", required=True)
 parser.add_argument("--port", default=5985)
-parser.add_argument("--socksport", default=1234)
+parser.add_argument("--socksport", default='socks5h://unixsocket/tmp/mysock.sock')
 parser.add_argument("--username", required=True)
 parser.add_argument("--password", required=True)
 parser.add_argument("--protocol", default="http")
@@ -34,7 +34,7 @@ p = Protocol(
     username=args.username,
     password=args.password,
     server_cert_validation='ignore',
-    proxy='socks5h://localhost:'+str(args.socksport))
+    proxy=args.socksport)
 
 # Load script to env vars.
 shell_id = p.open_shell(env_vars=dict(WINRM_SCRIPT=script))
@@ -47,8 +47,7 @@ p.cleanup_command(shell_id, command_id)
 p.close_shell(shell_id)
 print("status_code:",status_code)
 print("std_out:")
-#print(str(std_out, 'ascii')) # python3 only
-print(str(std_out).encode('ascii'))
+print(str(std_out, 'ascii')) # python3 only
 print("std_err:")
-#print(str(std_err, 'ascii')) # python3 only
-print(str(std_err).encode('ascii'))
+print(str(std_err, 'ascii')) # python3 only
+
